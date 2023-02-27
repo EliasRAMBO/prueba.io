efbc88a7ef31eb23b4673097f220c9752eb605e4
EliasRAMBO <69161931+EliasRAMBO@users.noreply.github.com>
2023-02-27 01:53:13 -0300

    a

---
+++ datosdel_cv.js
@@ -0,0 +1,18 @@
+
+document.getElementById("datos_personales").addEventListener("mousemove", function () {
+    document.getElementById("datos").innerHTML = "Jacqueline Rojas";
+  });
+
+document.getElementById("estudios").addEventListener("mousemove", function () {
+  document.getElementById("datos").innerHTML = "Universitario incompleto";
+  });
+document
+  .getElementById("experiencias").addEventListener("mousemove", function () {
+    document.getElementById("datos").innerHTML = "Atencion a proveedores y clientes  ";
+  });
+  
+document.getElementById("contactos").addEventListener("mousemove", function () {
+  document.getElementById("datos").innerHTML = "mi mail: aurojaq@gmail.com";
+  });
+
+  
